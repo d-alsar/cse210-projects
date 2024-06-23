a7c84b2a7d1783c129b7588ae3d4664e43f539ad
d-alsar <doglasreyes290@gmail.com>
2024-06-22 18:30:59 -0600

    completed entry class

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,11 +1,23 @@
-public class Entry
-{
-    public string _date;
-    public string _promptText;
-    public string _entryText;
+using System;
 
-    public void Display()
+namespace DailyJournal
+{
+    public class Entry
     {
-        
+        public string Date { get; private set; }
+        public string PromptText { get; private set; }
+        public string EntryText { get; private set; }
+
+        public Entry(string date, string promptText, string entryText)
+        {
+            Date = date;
+            PromptText = promptText;
+            EntryText = entryText;
+        }
+
+        public override string ToString()
+        {
+            return $"Date: {Date} - Prompt: {PromptText}\n{EntryText}\n";
+        }
     }
 }

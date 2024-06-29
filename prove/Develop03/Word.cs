06d853140547124b802c7972e3a7be927d11b6bd
d-alsar <doglasreyes290@gmail.com>
2024-06-29 13:23:09 -0600

    Completed the word class

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace DailyScripture
+{
+    class Word
+    {
+        public string Text { get; private set; }
+        public bool IsHidden { get; private set; }
+
+        public Word(string text)
+        {
+            Text = text;
+            IsHidden = false;
+        }
+
+        public void Hide()
+        {
+            IsHidden = true;
+        }
+    }
+}
